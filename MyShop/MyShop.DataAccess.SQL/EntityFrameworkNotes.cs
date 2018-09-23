1ed68a085c4f53d5192a6c348c28f59ca75c78bf
Tuhafeni <tmtangula11@gmail.com>
2018-09-23 16:05:15 +0200

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/EntityFrameworkNotes.cs
@@ -0,0 +1,36 @@
+﻿/*
+
+THIS DOCUMENT CONTAINS GENERAL NOTES
+
+ENTITY FRAMEWORK:
+- Install (via NuGet Packages or Package Manager Console)
+- For Code First MIgrations:
+    - Define the connectionString in both web.config and app.config
+        - app.config in the projects which will contain migrations
+        - webconfig in the project to be run
+    - In Package Manager Console:
+        - Run: Enable-Migrations (to enable migrations)
+            - This creates a Migrations folder and a Configuration.cs file
+        - Run: Add-Migration <MigrationName>
+            - This prepares a .cs file to help writing/uopdating the db
+        - Run: Update-Database
+            - This implements the migration
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
