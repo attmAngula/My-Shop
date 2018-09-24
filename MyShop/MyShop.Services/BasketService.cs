98d57508b9541f5af4e6bbfa5d5f7900e32525cf
Tuhafeni <tmtangula11@gmail.com>
2018-09-24 16:36:33 +0200

    Added the Basket Service

---
+++ MyShop/MyShop.Services/BasketService.cs
@@ -0,0 +1,138 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Services
+{
+    public class BasketService
+    {
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+
+        public const string BasketSessionName = "eCommerceBasket";
+
+        // ctor
+        public BasketService(IRepository<Product> productContext, IRepository<Basket> basketContext)
+        {
+            this.basketContext = basketContext;
+            this.productContext = productContext;
+        }
+
+        /// <summary>
+        /// Gets an existing basket from the cookies. Creates a new
+        /// basket if no basket exists in the cookies.
+        /// </summary>
+        /// <param name="httpContext">
+        /// Context from the browser.
+        /// </param>
+        /// <param name="createIfNull">
+        /// True or false. Option to create a cookie if none is found.
+        /// </param>
+        /// <returns>
+        /// Returns a Basket object.
+        /// </returns>
+        private Basket GetBasket(HttpContextBase httpContext, bool createIfNull)
+        {
+            HttpCookie cookie = httpContext.Request.Cookies.Get(BasketSessionName);
+
+            Basket basket = new Basket();
+
+            if (cookie != null)
+            {
+                string basketId = cookie.Value;
+                if (!string.IsNullOrEmpty(basketId))
+                {
+                    basket = basketContext.Find(basketId);
+                }
+                if (createIfNull)
+                {
+                    basket = CreateNewBasket(httpContext);
+                }
+            }
+            else
+            {
+                if (createIfNull)
+                {
+                    basket = CreateNewBasket(httpContext);
+                }
+            }
+
+            return basket;
+
+        } // end of GetBasket()
+
+        /// <summary>
+        /// Creates a new basket and adds it to the cookies.
+        /// </summary>
+        /// <param name="httpContext">
+        /// Context from the browser.
+        /// </param>
+        /// <returns>
+        /// Returns the created basket.
+        /// </returns>
+        private Basket CreateNewBasket(HttpContextBase httpContext)
+        {
+            Basket basket = new Basket();
+            basketContext.Insert(basket);
+            basketContext.Commit();
+
+            HttpCookie cookie = new HttpCookie(BasketSessionName);
+            cookie.Value = basket.Id;
+            cookie.Expires = DateTime.Now.AddDays(1);
+            httpContext.Response.Cookies.Add(cookie);
+
+            return basket;
+        }
+
+        /// <summary>
+        /// Adds an item to the basket.
+        /// </summary>
+        /// <param name="httpContext">
+        /// Context from the browser.
+        /// </param>
+        /// <param name="productId">
+        /// Id of the product to be added.
+        /// </param>
+        public void AddToBasket(HttpContextBase httpContext, string productId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if (item == null)
+            {
+                item = new BasketItem()
+                {
+                    BasketId = basket.Id,
+                    ProductId = productId,
+                    Quantity = 1
+                };
+
+                basket.BasketItems.Add(item);
+            }
+            else
+            {
+                item.Quantity = item.Quantity + 1;
+            }
+
+            basketContext.Commit();
+        }
+
+        public void RemoveFromBasket(HttpContextBase httpContext, string itemId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
+
+            if (item != null)
+            {
+                basket.BasketItems.Remove(item);
+                basketContext.Commit();
+            }
+        }
+
+    }
+}
---
+++ MyShop/MyShop.Services/GeneralNotes.cs
@@ -0,0 +1,12 @@
+﻿/*
+
+GENERAL NOTES ON MyShop.Services
+
+    - The main idea of services is to implement business logic
+        - Other projects are mainly dealing with data
+        - Also to implement saving and reading cookies from the users browser
+        
+    Note: The data models needed by the services will be implented in
+            the controllers.
+
+ */
